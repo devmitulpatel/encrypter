01485f6c331c74a4f5941ce43842121a34e88e87
mitulclassy <68455017+mitulclassy@users.noreply.github.com>
2020-07-29 18:03:30 +0530

    update

--- src/Core/Encrypter.php
+++ src/Core/Encrypter.php
@@ -2,4 +2,115 @@
 
 
 namespace M\Core;
-set_time_limit(300);set_time_limit(300);
+ini_set('memory_limit', '-1');
+
+
+set_time_limit(-1);
+class Encrypter
+{
+
+
+    public static function test(){
+
+
+        $a="0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+=-";
+
+        $d=str_split($a);
+
+        $array=[];
+
+        for ($x=0;$x<100;$x++){
+            $array[$x]=[];
+            foreach ($d as $k){
+               $rByte=random_bytes(1);
+
+               while (array_key_exists($rByte,$array[$x])){
+                   $rByte=random_bytes(1);
+
+               }
+
+                $array[$x][$rByte]=$k;
+            }
+
+          //  var_dump(count($array[$x]));
+        }
+        file_put_contents(str_replace('\\','/',__DIR__).'/algo.bin', serialize($array));
+    }
+
+
+    public static function encrypt($str=""){
+        $rstr="";
+        $data=  unserialize(file_get_contents(str_replace('\\','/',__DIR__).'/algo.bin'));
+
+
+
+
+        $strEx=str_split($str);
+        $strSecret=[];
+
+
+        $randdomKey=rand(0,count($data)-1);
+
+
+        $sData=$data[$randdomKey];
+        while(strlen($randdomKey) !=2){
+            $randdomKey=rand(0,count($data)-1);
+        }
+
+        $randdomKeyEx=str_split($randdomKey);
+
+
+
+        foreach ($strEx as  $k){
+            if(in_array($k,$sData))$strSecret[]=array_search($k,$sData);
+        }
+
+
+        $final=[
+          reset($randdomKeyEx),
+            $strSecret,
+          end($randdomKeyEx)
+
+        ];
+
+        dd($final);
+
+
+       // var_dump(base64_encode($str));
+        //var_dump( implode('_',[$salt,$rstr,$salt2]) );
+
+        return $rstr;
+
+    }
+
+    public static  function  decrypt($str=""){
+        $rstr="";
+
+      //  var_dump(password_verify($str));
+
+        $rstr=base64_decode ($str);
+      // var_dump($rstr);
+
+        return $rstr;
+    }
+
+    public static  function  encryptLimit($str="",$min=30){
+
+        $rstr="";
+
+        return $rstr;
+
+    }
+
+    public static  function  decryptLimit($str="",$min=30){
+
+        $rstr="";
+
+        return $rstr;
+
+    }
+
+
+
+
+}
